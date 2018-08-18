8ddd8d2ddd7f1e4ca145652c12ce8dc26079d890
CapCap_ghostcorn <CapCap_ghostcorn@git.cloud.tencent.com>
2018-08-19 00:37:24 +0800

    update zil

--- es6/zil/index.js
+++ es6/zil/index.js
@@ -8,14 +8,14 @@ class Zil {
     this.mapObjectToMethods(self)
   }
 
-  mapObjectToMethods = (self) => {
+  mapObjectToMethods = (obj) => {
     ZilObjects.map((data) => {
       const zilMethod = new Method(data)
       zilMethod.setMessanger(this.messanger)
       const zilKey = data.name
       const zilObject = {}
       zilObject[zilKey] = zilMethod
-      console.log(self)
+      console.log(obj)
       return zilObject
       // return Object.assign(self, zilObject)
     })
