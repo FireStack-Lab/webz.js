ca325238826edf498ddaa786d12b239bfe8bc236
CapCap_ghostcorn <CapCap_ghostcorn@git.cloud.tencent.com>
2018-08-19 01:55:43 +0800

    update method.js

--- es6/util/method.js
+++ es6/util/method.js
@@ -26,9 +26,9 @@ class Method {
       return (args, callback) => {
         this.validateArgs(args)
         if (callback) {
-          this.messanger.sendAsync(this.call, args, callback)
+          this.messanger.sendAsync({ method: this.call, params: args }, callback)
         }
-        this.messanger.send(this.call, args)
+        this.messanger.send({ method: this.call, params: args })
       }
     }
   }
