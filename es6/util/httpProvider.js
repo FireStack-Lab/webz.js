f510450accade1722bc93386df8c7c1971567ec1
CapCap_ghostcorn <CapCap_ghostcorn@git.cloud.tencent.com>
2018-08-19 04:51:01 +0800

    > update httpProvider.js

--- es6/util/httpProvider.js
+++ es6/util/httpProvider.js
@@ -33,7 +33,6 @@ class HttpProvider {
   send = async (payload) => {
     try {
       const response = await this.axios.post(this.url, JSON.stringify(payload))
-      console.log({ response, payload })
       const { data, status } = response
       if (data.result && status === 200) {
         return data.result
