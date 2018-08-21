e2282c6d36a5f2a98b00428bb0293a0756fda303
CapCap_ghostcorn <CapCap_ghostcorn@git.cloud.tencent.com>
2018-08-21 14:05:30 +0800

    > update .babelrc

--- es6/index.js
+++ es6/index.js
@@ -2,7 +2,7 @@ import Webz from './webz';
 
 // dont override global variable
 if (typeof window !== 'undefined' && typeof window.Webz === 'undefined') {
-  window.Webz = Webz.default;
+  window.Webz = Webz;
 }
 
 export default Webz;
